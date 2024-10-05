a37b4888ac41d01cdf784ddf74ed5c0c77245c8b
renshimosawa <polaris2760@gmail.com>
2024-10-06 07:53:28 +0900

    add redirect

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,25 @@ const nextConfig = {
   experimental: {
     scrollRestoration: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/map/hachinohe",
+        destination: "/maps/hachinohe",
+        permanent: true,
+      },
+      {
+        source: "/map/kyoto",
+        destination: "/maps/kyoto",
+        permanent: true,
+      },
+      {
+        source: "/map/morioka",
+        destination: "/maps/morioka",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
